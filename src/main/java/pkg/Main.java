e59e288158126569e41088b162aa1002d082d5c2
ahmedelahwany <61867082+ahmedelahwany@users.noreply.github.com>
2020-04-08 11:24:21 +0200

    Add files via upload

---
+++ src/main/java/pkg/Main.java
@@ -0,0 +1,20 @@
+package pkg;
+
+import org.tinylog.Logger;
+
+
+public class Main
+{
+    public static void main(String[] args) {
+        Logger.error("This is an ERROR message");
+        Logger.warn("This is WARN string");
+        Logger.info("This is an INFO");
+        Logger.debug("This is a DEBUG message");
+        Logger.trace("This is a TRACE message");
+
+//        Logger.info("User's name is " + System.getProperty("user.name"));
+
+        Logger.info("User's name is {}", System.getProperty("user.name"));
+        Logger.error(new RuntimeException("OOPS"), "Something is wrong");
+    }
+}
